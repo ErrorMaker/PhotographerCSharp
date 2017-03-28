59abc96c81ca547434838c7245e1db8a9ef86ba1
Scott Stamp <scottstamp851@gmail.com>
2017-03-28 04:46:17 -0230

    Add project files.

---
+++ Photographer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Photographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
